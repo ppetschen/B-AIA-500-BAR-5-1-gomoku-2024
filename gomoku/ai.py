ae882268694b662d104b92f1ab3e08b1b70bfe64
Lucia Jimenez <lucia.jimenez-diaz@epitech.eu>
2024-11-24 19:10:04 +0100

    [ADD] back to working ai commit

--- gomoku/ai.py
+++ gomoku/ai.py
@@ -1,5 +1,4 @@
-from concurrent.futures import ThreadPoolExecutor
-from typing import Tuple
+from typing import Optional, Tuple
 from gomoku.game_board import GameBoard
 import time
 import threading
@@ -23,18 +22,18 @@ class AI:
         self.game_board: GameBoard = game_board
         self.best_move: BestMove = BestMove()
         self.scores = {
-            0: 0.1,
+            0: 1,
             1: 5,
-            2: 50,
-            3: 300,
+            2: 20,
+            3: 150,
             4: 15000
         }
         self.opponent_scores = {
-            0: 0.1,
+            0: 1,
             1: 5,
-            2: 50,
-            3: 250,
-            4: 2500
+            2: 20,
+            3: 150,
+            4: 1500
         }
 
     def calculate_move(self, player: str) -> Tuple[int, int]:
@@ -57,85 +56,74 @@ class AI:
 
     def simulate_move(self) -> None:
         evaluated_moves = []
-        with ThreadPoolExecutor() as executor:
-            futures = [
-                executor.submit(self.evaluate_position, j, i)
-                for i in range(self.game_board.size)
-                for j in range(self.game_board.size)
-                if self.game_board.board[i][j] == self.game_board.EMPTY
-            ]
-            for future in futures:
-                j, i, score = future.result()
-                evaluated_moves.append((j, i, score))
-                if score > self.best_move.score:
-                    self.best_move.x = j
-                    self.best_move.y = i
-                    self.best_move.score = score
-                    if score >= 15000:
-                        return
+        for i in range(self.game_board.size):
+            for j in range(self.game_board.size):
+                if self.game_board.board[i][j] == self.game_board.EMPTY:
+                    score = self.evaluate_position(j, i)
+                    evaluated_moves.append((j, i, score))
+                    if score > self.best_move.score:
+                        self.best_move.x = j
+                        self.best_move.y = i
+                        self.best_move.score = score
         evaluated_moves.sort(key=lambda move: move[2], reverse=True)
         top_moves = evaluated_moves[:5] + evaluated_moves[-5:]
         self.game_board.set_top_moves(top_moves)
 
-    def evaluate_position(self, x: int, y: int) -> Tuple[int, int, int]:
+    def evaluate_position(self, x: int, y: int) -> int:
         score = 0
-        isBlocked = True
         for direction in (
             (1, 0),
             (0, 1),
             (1, 1),
             (1, -1),
         ):
-            directionScore, directionBlocked = self.evaluate_direction(x, y, direction)
-            score += directionScore
-            if not directionBlocked:
-                isBlocked = False
-        if isBlocked:
-            score = score * 0.01
-        return x, y, score
+            score += self.evaluate_direction(x, y, direction)
+        return score
 
     def evaluate_direction(self, x: int, y: int, direction: Tuple[int, int]) -> int:
-        count_self, selfHasSpace, selfIsBlocked, selfPreBlocked = self.count_player_in_direction(x, y, direction, self.current_player)
-        count_opponent, opponentHasSpace, opponentIsBlocked, opponentPreBlocked = self.count_player_in_direction(x, y, direction, self.opponent_player)
+        count_self_1, selfHasSpace_1, selfIsBlocked_1 = self.count_player_in_direction(x, y, direction, self.current_player)
+        count_self_2, selfHasSpace_2, selfIsBlocked_2 = self.count_player_in_direction(x, y, (-direction[0], -direction[1]), self.current_player)
+        count_self = count_self_1 + count_self_2
+        selfIsBlocked = selfIsBlocked_1 and selfIsBlocked_2
+        selfIsHalfBlocked = selfIsBlocked_1 or selfIsBlocked_2
+        selfHasSpace = selfHasSpace_1 or selfHasSpace_2
+        selfHalfSpace = selfHasSpace_1 and selfHasSpace_2
 
-        selfScore = self.scores[count_self] * (0.8 if selfHasSpace else 1) * (0.5 if selfIsBlocked else 1) * (0.7 if selfPreBlocked else 1)
-        opponentScore = self.opponent_scores[count_opponent] * (0.8 if opponentHasSpace else 1) * (0.5 if opponentIsBlocked else 1) * (0.7 if opponentPreBlocked else 1)
+        count_opponent_1, opponentHasSpace_1, opponentIsBlocked_1 = self.count_player_in_direction(x, y, direction, self.opponent_player)
+        count_opponent_2, opponentHasSpace_2, opponentIsBlocked_2 = self.count_player_in_direction(x, y, (-direction[0], -direction[1]), self.opponent_player)
+        count_opponent = count_opponent_1 + count_opponent_2
+        opponentIsBlocked = opponentIsBlocked_1 and opponentIsBlocked_2
+        opponentIsHalfBlocked = opponentIsBlocked_1 or opponentIsBlocked_2
+        opponentHasSpace = opponentHasSpace_1 or opponentHasSpace_2
+        opponentHalfSpace = opponentHasSpace_1 and opponentHasSpace_2
 
-        return selfScore + opponentScore, selfIsBlocked and opponentIsBlocked
+        selfScore = self.scores[count_self] * (0.8 if selfHasSpace else 1) * (0.9 if selfHalfSpace else 1) * (0.5 if selfIsBlocked else 1) * (0.9 if selfIsHalfBlocked else 1)
+        opponentScore = self.opponent_scores[count_opponent] * (0.8 if opponentHasSpace else 1) * (0.9 if opponentHalfSpace else 1) * (0.5 if opponentIsBlocked else 1) * (0.9 if opponentIsHalfBlocked else 1)
+
+        return selfScore + opponentScore
 
     def count_player_in_direction(
         self, x: int, y: int, direction: Tuple[int, int], player: str
     ) -> Tuple[int, bool, bool]:
-        max_count = 0
+        count = 0
         hasSpace = False
-        isBlocked = True
-        groupBlocked = False
-        preBlocked = False
-
-        for start in range(-4, 1):
-            count = 0
-            spaceInMiddle = False
-            if groupBlocked:
-                preBlocked = True
-            groupBlocked = False
-            for i in range(5):
-                new_x = x + (start + i) * direction[0]
-                new_y = y + (start + i) * direction[1]
-                if not self.game_board.is_inside_board(new_x, new_y):
-                    groupBlocked = True
-                    break
-                if self.game_board.board[new_y][new_x] == player:
-                    count += 1
-                    if spaceInMiddle:
-                        hasSpace = True
-                elif self.game_board.board[new_y][new_x] != self.game_board.EMPTY:
-                    groupBlocked = True
-                    break
-                else:
-                    if count > 0:
-                        spaceInMiddle = True
-            if not groupBlocked:
-                isBlocked = False
-                max_count = max(max_count, count)
-
-        return max_count, hasSpace, isBlocked, preBlocked
+        spaceInMiddle = False
+        isBlocked = False
+        for i in range(1, 5):
+            new_x = x + i * direction[0]
+            new_y = y + i * direction[1]
+            if not self.game_board.is_inside_board(new_x, new_y):
+                isBlocked = True
+                break
+            if self.game_board.board[new_y][new_x] == player:
+                count += 1
+                if spaceInMiddle:
+                    hasSpace = True
+            elif self.game_board.board[new_y][new_x] != self.game_board.EMPTY:
+                isBlocked = True
+                if spaceInMiddle:
+                    hasSpace = True
+                break
+            else:
+                spaceInMiddle = True
+        return count, hasSpace, isBlocked
